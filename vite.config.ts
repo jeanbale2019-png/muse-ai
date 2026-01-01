4b444c8c2b1bce7fd2edac6ce2fa505f88affbe7
Jean BALE <jeanbale2019@gmail.com>
2026-01-01 23:09:24 +0400

    feat: Add Firebase initialization
    Integrates Firebase into the application for analytics and future features. Initializes Firebase with configuration details and imports necessary SDKs.

--- vite.config.ts
+++ vite.config.ts
@@ -29,3 +29,24 @@ export default defineConfig({
     }
   }
 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClOUFt9aCDWEvDoG5u6kAQ-nvG_Byh_LI",
+  authDomain: "social-muse-f29f9.firebaseapp.com",
+  projectId: "social-muse-f29f9",
+  storageBucket: "social-muse-f29f9.firebasestorage.app",
+  messagingSenderId: "628842678494",
+  appId: "1:628842678494:web:1d85cd42a4e95334745a4f",
+  measurementId: "G-MGG281987N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
